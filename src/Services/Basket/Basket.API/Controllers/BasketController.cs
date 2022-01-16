d3b9c3ed4dad24ebcd42f6e0180d76f80f09f308
SylwiaLeung <longdepati@gmail.com>
2022-01-16 19:20:33 +0100

    Configured all services, dockerized them and oceloted them

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -2,6 +2,8 @@
 using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
@@ -15,15 +17,15 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly DiscountGrpcService _discountGrpcService;
-        //private readonly IPublishEndpoint _publishEndpoint;
-        //private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        public BasketController(IPublishEndpoint publishEndpoint, IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
-            //_publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
-            //_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -55,26 +57,26 @@ namespace Basket.API.Controllers
             return Ok();
         }
 
-        //[Route("[action]")]
-        //[HttpPost]
-        //[ProducesResponseType((int)HttpStatusCode.Accepted)]
-        //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        //public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
-        //{
-        //    var basket = await _repository.GetBasket(basketCheckout.UserName);
-        //    if (basket == null)
-        //    {
-        //        return BadRequest();
-        //    }
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
 
-        //    var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
-        //    eventMessage.TotalPrice = basket.TotalPrice;
-        //    await _publishEndpoint.Publish(eventMessage);
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);
 
-        //    // remove the basket
-        //    await _repository.DeleteBasket(basket.UserName);
+            // remove the basket
+            await _repository.DeleteBasket(basket.UserName);
 
-        //    return Accepted();
-        //}
+            return Accepted();
+        }
     }
 }
